a654e98397e1e69195b8257c118648af507790f8
maelawre <maelawre@ucsc.edu>
2025-04-03 12:01:42 -0700

    Add files via upload
    no longer emptyyy

---
+++ hello-world.py
@@ -0,0 +1,18 @@
+#This program will write
+# Hello world!
+
+
+#This function tells the computer to print
+# Hello World!
+def PrintHelloWorld(): 
+	print("Hello World!")
+
+#This defines our main()
+# function for our program
+def main():
+	PrintHelloWorld()
+
+#When we run the program, 
+#This executes first
+if __name__ == "__main__":
+	main()
