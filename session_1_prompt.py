10305203ffdf8616fc4e12f1e04d40674b648892
maelawre <maelawre@ucsc.edu>
2025-04-17 13:11:14 -0700

    Add files via upload

---
+++ session_1_prompt.py
@@ -0,0 +1,8 @@
+def main():
+	print("My name is Maya Lawrence and my pronouns are she/her.")
+	print("My favorite movie is Ponyo")
+	print("My favorite food is tomato soup.")
+
+if __name__ == '__main__':
+	main()
+
